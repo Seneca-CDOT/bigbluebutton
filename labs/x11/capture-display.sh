7f873b0b33bc19123419269fc64f0a29d1919438
Matthew Marangoni <matthew.marangoni@gmail.com>
2018-09-28 17:49:01 -0400

    minor updates to improve variables

--- labs/x11/capture-display.sh
+++ labs/x11/capture-display.sh
@@ -1,27 +1,24 @@
 #!/usr/bin/bash
 
 function cleanup {
+echo "Cleaning up"
+kill $browser_pid $xserver_pid >&/dev/null
+
 #if [ -f /tmp/.X${DISPLAY:1}-lock ]; then
-#  kill $xserver_pid
 #  rm -rf /tmp/.X${DISPLAY:1}-lock
 #fi
 
-kill $browser_pid
-
-echo "Exiting..."
+echo "Exiting"
 }
 trap cleanup SIGINT SIGTERM EXIT
 
-if [ -f /tmp/foo.mp4 ]; then
-  rm -rf /tmp/foo.mp4
-fi
-
 export DISPLAY=:1
-duration=5
+OUTFILE=/tmp/capture.mp4
+DURATION=5
 
 # Start the X server
 Xvfb $DISPLAY -screen 0 640x480x24 -ac &
-#xserver_pid=$!
+xserver_pid=$!
 
 # Run a browser to capture
 firefox &
@@ -29,5 +26,8 @@ firefox &
 browser_pid=$!
 
 # Capture display with ffmpeg for a set duration
-(sleep $duration; echo q) | ffmpeg -framerate 12 -s 640x480 -f x11grab -i $DISPLAY -g 24 -preset veryfast -tune zerolatency -r 12 -c:v libx264 -crf 24 -pix_fmt yuv420p /tmp/foo.mp4
+if [ -f $OUTFILE ]; then
+  rm $OUTFILE
+fi
+ffmpeg -framerate 12 -s 640x480 -f x11grab -t $DURATION -i $DISPLAY -g 24 -preset veryfast -tune zerolatency -r 12 -c:v libx264 -crf 24 -pix_fmt yuv420p $OUTFILE
 
